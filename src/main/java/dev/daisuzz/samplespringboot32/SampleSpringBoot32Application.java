76f23b3a4d6580dd78491ece70bdf3d90105b90d
daisuzz <daisaku.rugby@gmail.com>
2024-02-25 16:44:17 +0900

    first commit

---
+++ src/main/java/dev/daisuzz/samplespringboot32/SampleSpringBoot32Application.java
@@ -0,0 +1,12 @@
+package dev.daisuzz.samplespringboot32;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleSpringBoot32Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleSpringBoot32Application.class, args);
+    }
+}
---
+++ src/test/java/dev/daisuzz/samplespringboot32/SampleSpringBoot32ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.daisuzz.samplespringboot32;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleSpringBoot32ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
